b8f342d5a70f4499aca076c1d860162e8958a9a8
miraclennadike <miracle.nnadike@pau.edu.ng>
2025-10-25 00:35:44 +0100

    week-4

---
+++ week-4/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\mirac\OneDrive\Documents\m.nnadikeCOS101\week-4\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\mirac\OneDrive\Documents\m.nnadikeCOS101\week-4\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\mirac\OneDrive\Documents\m.nnadikeCOS101\week-4\practice_6\target\debug\practice_6.exe: C:\Users\mirac\OneDrive\Documents\m.nnadikeCOS101\week-4\practice_6\src\main.rs
